b5df4d09b150e56e5a2be8902d650ccacaf70c31
Raquel <rg61@alu.ua.es>
2026-04-28 17:10:07 +0200

    pagina principal ia

--- lib/screens/recipes/recipes_screen.dart
+++ lib/screens/recipes/recipes_screen.dart
@@ -6,11 +6,62 @@ class RecipesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("CocinIA")),
-      body: const Center(
-        child: Text(
-          "Aquí aparecerán recetas generadas por IA",
-          style: TextStyle(fontSize: 18),
+      appBar: AppBar(
+        title: const Text("Recetas"),
+        backgroundColor: const Color(0xFF537e5e),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xFF537e5e),
+        onPressed: () {
+          // Aquí luego abrirás cámara / IA
+          print("Abrir IA");
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            
+            SizedBox(height: 10),
+
+            Text(
+              "Recomendaciones diarias",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            Text(
+              "Esta sección ofrece recetas diarias generadas mediante "
+              "inteligencia artificial.\n\n"
+              "Podrás descubrir platos en función de tus ingredientes o "
+              "subiendo una imagen desde tu dispositivo.",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black87,
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            Center(
+              child: Text(
+                "Las recetas aparecerán aquí",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black54,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
